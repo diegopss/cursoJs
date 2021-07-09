9f133044be0e6d1b778761571a686355e25dec83
diegopss <die-gops@hotmail.com>
2021-07-09 15:29:44 -0300

    exercicio 16 concluido

--- cursoJS/exercicio16/script.js
+++ cursoJS/exercicio16/script.js
@@ -2,20 +2,37 @@ function contar(){
 var inc = document.querySelector('input#inc')
 var fim = document.querySelector('input#fim')
 var pas = document.querySelector('input#pas')
-var result = ""
 var res = document.querySelector('div#res')
 var i = Number(inc.value)
 var f = Number(fim.value)
 var p = Number(pas.value)
+var emoji 
 
-for (x = i; x < f; x += p ){
+if (inc.value.length == 0 || fim.value.length == 0 || pas.value.length == 0 ) {
+    res.innerHTML = `[Erro] prencha os campos`
+}else{
+    if(p < 1){
+        alert('considerando o passo como 1')
+        p = 1        
+    }
+    if(i<f){
+    for (var x = i; x <= f; x += p ){
 
-result = x 
+        res.innerHTML += `${x} &#128073`
 
- res.innerHTML = `${x}`
+    }
+        res.innerHTML += `&#128076`
+} else if (i>f) {
+    for (var x = i; x >= f; x -= p ){
 
+        res.innerHTML += `${x} &#128073`
+
+    }
+        res.innerHTML += `&#128076`
+    
+}       
+    
 
 }
-
-
 }
+
